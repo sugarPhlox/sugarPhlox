9b6ed5dae83a49fe0aed79ed346a38db4cac45a9
Cara <cara@daftlabs.com>
2015-08-19 16:10:12 -0400

    fix to navbar js, addition of nav links, addition of social media links, almost there

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,6 +1,6 @@
 (function(){
   $(window).scroll(function() {     
-    if($(this).scrollTop() > $(window).height()) { 
+    if($(this).scrollTop() > ($(window).height() - 60)) { 
         $('.navbar').addClass('opaque');
     } else {
         $('.navbar').removeClass('opaque');
